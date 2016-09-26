141f4c0e349450051776225d3addc57f5dbab287
hiltanl <hiltan@126.com>
2016-09-26 15:20:11 +0800

    ASP.Net学习SportStore开发

---
+++ Study/SportsStore/SportsStore/Models/Respository/EFDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.Models.Respository
+{
+    public class EFDbContext:DbContext
+    {
+        public DbSet<Product> Proudcts { get; set; }
+    }
+}
